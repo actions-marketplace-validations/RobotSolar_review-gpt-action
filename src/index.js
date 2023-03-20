0496beece7a0c11dc70e545966131775af43038e
connor <connor@dunamu.com>
2023-03-20 23:16:49 +0900

    chore: log 추가

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 const { run } = require('@probot/adapter-github-actions');
 
 run((app) => {
-  app.log('Yay! The app was loaded!');
+  app.on('pull_request.opened', async (context) => {
+    const repo = context.repo();
 
-  app.on('issues.opened', async (context) => {
-    return context.octokit.issues.createComment(
-      context.issue({ body: 'Hello, World!' })
-    );
+    const pullRequest = context.payload.pull_request;
+
+    if (
+      pull_request.state === 'closed' ||
+      pull_request.locked ||
+      pull_request.draft
+    ) {
+      return 'invalid event payload';
+    }
+
+    const { data } = await context.octokit.repos.compareCommits({
+      owner: repo.owner,
+      repo: repo.repo,
+      base: pullRequest.base.sha,
+      head: pullRequest.head.sha,
+    });
+
+    const { files: changedFiles, commits } = data;
+
+    if (!changedFiles?.length) {
+      return 'no change';
+    }
+
+    for (let i = 0; i < changedFiles.length; i++) {
+      app.log(changedFiles[i]);
+    }
+    return;
+    // return context.octokit.issues.createComment(
+    //   context.issue({ body: 'Hello, World!' })
+    // );
   });
 });
